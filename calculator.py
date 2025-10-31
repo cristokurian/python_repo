55b3d5ad529c67a9f7e88abd2a8379dd62109f14
cristokurian <cristokurian4@gmail.com>
2025-10-31 20:22:57 +0530

    made a calculator

---
+++ calculator.py
@@ -0,0 +1,9 @@
+n=int(input("enter 1dt no:"))
+o=input("enter the operator:")
+n1=int(input("enter 2nd no:"))
+if o=="+":
+    print(n+n1)
+elif o=="-":
+    print(n-n1) 
+else:
+    print("enter valid operator")       
